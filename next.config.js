b21f4641085fb02e7340b056707adc127517d330
pheralb <pablohdez4340@gmail.com>
2022-06-03 11:53:50 +0100

    ⚒️ Preparate boilerplate v2 - Typescript + React 18.

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
-const withPWA = require("next-pwa");
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
+}
 
-module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === "development",
-  },
-  nextConfig,
-});
+module.exports = nextConfig
--- pages/404.js
+++
@@ -1,8 +0,0 @@
-import Error from 'components/error';
-import React from 'react';
-
-const Error404 = () => {
-  return <Error />;
-};
-
-export default Error404;
