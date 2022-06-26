fd170340bf519c4ebbbd9780ca01eac5764896d9
pheralb <pablohdez4340@gmail.com>
2022-06-26 16:35:05 +0100

    ⚒️ Add footer.

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { LayoutProps } from "@/interfaces/components";
 import { Container } from "@chakra-ui/react";
 import Header from "./header";
+import Footer from "./footer";
 
 const Index = ({ children }: LayoutProps) => {
   return (
     <>
       <Header />
       <Container maxW="70%" mt="5">{children}</Container>
+      <Footer />
     </>
   );
 };
