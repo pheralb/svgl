89506de0e93abd22cf413b84d908aa7b5f1c65fb
pheralb <pablohdez4340@gmail.com>
2023-03-15 11:28:22 +0000

    🛠️ Add tailwindcss.

---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        dark: '#161616'
+      },
+      fontFamily: {
+        sans: ['General-Sans', 'sans-serif']
+      },
+      fontSize: {
+        mini: '14px'
+      }
+    }
+  },
+  plugins: []
+};
