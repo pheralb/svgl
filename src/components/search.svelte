7e3b2e0636eb61e7c4ab4afeaf5b8d556da7cade
pheralb <pablohdez4340@gmail.com>
2023-03-19 16:44:29 +0000

    🛠️ Search input improvements.

--- src/components/search.svelte
+++ src/components/search.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
   export let searchTerm: string;
+  export let placeholder: string = "Search...";
 </script>
 
 <input
   type="text"
-  placeholder="Search..."
+  placeholder={placeholder}
   autocomplete="off"
   class="w-full rounded-md border border-neutral-800 bg-neutral-700/10 p-3 placeholder-neutral-500 focus:outline-none focus:ring-2 focus:ring-neutral-700"
   bind:value={searchTerm}
