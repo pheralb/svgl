a1ca62410b29551b72dbbe5b4c1571dd604706bb
pheralb <pablohdez4340@gmail.com>
2023-03-20 10:58:30 +0000

    🛠️ Add testing with vitest.

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,14 @@
 import { describe, it, expect } from 'vitest';
+import { svgs } from './data/svgs';
 
-describe('sum test', () => {
-  it('adds 1 + 2 to equal 3', () => {
-    expect(1 + 2).toBe(3);
+describe('Get svgs by category', () => {
+  it('should have a category named "Social"', () => {
+    expect(svgs.find((svg) => svg.category === 'Social')).toBeDefined();
+  });
+});
+
+describe('Get a specific svg', () => {
+  it('should have a svg named "Discord"', () => {
+    expect(svgs.find((svg) => svg.title === 'Discord')).toBeDefined();
   });
 });
