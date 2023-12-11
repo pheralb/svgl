2ebbd13cc6a4cd8d02efecf2606f0204b421fb54
Gaurav Chadha <gauravchadha1676@gmail.com>
2023-12-11 23:39:32 +0530

    add Rowy and BuildShip logos
    Signed-off-by: Gaurav Chadha <gauravchadha1676@gmail.com>

--- src/data/svgs.ts
+++ src/data/svgs.ts
@@ -1610,7 +1610,7 @@ export const svgs: iSVG[] = [
     category: 'Software',
     route: '/library/datadog.svg',
     url: 'https://www.datadoghq.com/'
-   },
+  },
   {
     id: 231,
     title: 'Randevum',
@@ -1618,4 +1618,18 @@ export const svgs: iSVG[] = [
     route: '/library/randevum.svg',
     url: 'https://www.randevum.co'
   },
+  {
+    id: 69114,
+    title: 'Rowy',
+    category: 'CMS',
+    route: '/library/rowy.svg',
+    url: 'https://www.rowy.io/'
+  },
+  {
+    id: 12251,
+    title: 'BuildShip',
+    category: 'AI',
+    route: '/library/buildship.svg',
+    url: 'https://buildship.com/'
+  }
 ];
