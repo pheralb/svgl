d7d10705e61eb6dd54702af844c1ff4d5949ae5a
pheralb <pablohdez4340@gmail.com>
2023-12-16 20:46:37 +0000

    ⚙️ Update check-size script with @actions/core pkg.

---
+++ check-size/index.js
@@ -0,0 +1,67 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { readdir, stat } = require('fs').promises;
+const { join } = require('path');
+
+// For GitHub Actions:
+const core = require('@actions/core');
+
+// 🔎 Settings:
+const dir = '../static/library';
+const sizeLimit = 20000; // 20kb;
+
+function convertBytes(bytes, format = 'KB') {
+  if (format === 'KB') {
+    return (bytes / 1024).toFixed(2) + ' KB';
+  } else if (format === 'MB') {
+    return (bytes / (1024 * 1024)).toFixed(2) + ' MB';
+  } else {
+    return 'Invalid format. Use "KB" or "MB".';
+  }
+}
+
+async function checkSize() {
+  const files = await readdir(dir);
+  let maxSize = 0;
+  let maxFiles = [];
+  let message = '';
+
+  try {
+    for (const file of files) {
+      const filePath = join(dir, file);
+      const stats = await stat(filePath);
+
+      if (stats.size > maxSize) {
+        maxSize = stats.size;
+        maxFiles.push({
+          filename: file,
+          size: maxSize
+        });
+      }
+    }
+
+    if (maxFiles.length === 0) {
+      message = `- ✅ All files are smaller than ${convertBytes(sizeLimit)}`;
+      console.log(message);
+      core.setOutput('message', message);
+    } else {
+      message = `- ❌ There are files bigger than ${convertBytes(sizeLimit)}.`;
+      throw new Error(message);
+    }
+  } catch (err) {
+    core.setFailed(message);
+  } finally {
+    if (maxFiles.length > 0) {
+      console.log('🔎 Files found:');
+      maxFiles.forEach((file) => {
+        console.log(`- 📄 ${file.filename} - ${convertBytes(file.size, 'KB')}`);
+      });
+    }
+    console.log('⚙️ Settings:');
+    console.log(`- 📁 Directory: ${dir}`);
+    console.log(`- 🧱 Size limit: ${convertBytes(sizeLimit)} bytes`);
+    console.log(`- 🔔 Max size found: ${convertBytes(maxSize, 'MB')}`);
+  }
+}
+
+// Run the function
+checkSize();
