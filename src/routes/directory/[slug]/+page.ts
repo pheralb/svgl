534681efa172beb578f95c7a262415f562b0fd9e
Angel Garcia <angelgarciaweb@gmail.com>
2023-12-18 16:46:34 -0400

    Add filter by category in svgs array

--- src/routes/directory/[slug]/+page.ts
+++ src/routes/directory/[slug]/+page.ts
@@ -3,6 +3,7 @@ import type { PageLoad } from './$types';
 
 import { svgs } from '@/data/svgs';
 import type { iSVG } from '@/types/svg';
+import type { tCategory } from '@/types/categories';
 
 export const load = (async ({ params }) => {
   const { slug } = params;
@@ -13,7 +14,11 @@ export const load = (async ({ params }) => {
   }
 
   // Filter out the svg with the matching slug:
-  const svgsByCategory = svgs.filter((svg: iSVG) => svg.category.toLowerCase() === slug);
+  const svgsByCategory = svgs.filter((svg: iSVG) => {
+    return svg.category.map((category) => category.toLowerCase()).includes(slug as tCategory);
+  });
+
+
 
   return {
     category: slug as string,
