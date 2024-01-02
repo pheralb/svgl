f17f4f1054f9d1f709afa50420cb0561aa1fe8f9
pheralb <pablohdez4340@gmail.com>
2024-01-02 10:06:14 +0000

    🐛 Update search input.

--- src/components/search.svelte
+++ src/components/search.svelte
@@ -17,7 +17,6 @@
       type="text"
       {placeholder}
       autocomplete="off"
-      autofocus={true}
       class="w-full border-b border-neutral-300 bg-white p-3 pl-11 placeholder-neutral-500 focus:outline-none focus:ring-1 focus:ring-neutral-300 dark:border-neutral-800 dark:bg-neutral-900 dark:focus:ring-neutral-700"
       bind:value={searchTerm}
       on:input
