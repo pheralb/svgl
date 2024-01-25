6b39aa4e70d52e68e20f2ccc53a096489aaa9675
pheralb <pablohdez4340@gmail.com>
2024-01-25 13:40:12 +0000

    💄 Add dialog UI component

---
+++ src/ui/dialog/dialog-description.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { Dialog as DialogPrimitive } from 'bits-ui';
+  import { cn } from '@/utils/cn';
+
+  type $$Props = DialogPrimitive.DescriptionProps;
+
+  let className: $$Props['class'] = undefined;
+  export { className as class };
+</script>
+
+<DialogPrimitive.Description class={cn('text-sm opacity-70 mb-2', className)} {...$$restProps}>
+  <slot />
+</DialogPrimitive.Description>
