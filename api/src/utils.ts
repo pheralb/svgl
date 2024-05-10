d395eea8b63c6f8b53ca29800b10fa1c4b8a1441
pheralb <pablohdez4340@gmail.com>
2024-05-10 19:23:26 +0100

    🔧 Initial api with Hono

---
+++ api/src/utils.ts
@@ -0,0 +1,15 @@
+import { ThemeOptions } from '../../src/types/svg';
+
+const fullUrl = 'https://svgl.app';
+
+export const addFullUrl = (value: string | ThemeOptions): string | ThemeOptions => {
+  if (typeof value === 'string') {
+    return `${fullUrl}${value}`;
+  } else if (typeof value === 'object') {
+    return {
+      light: `${fullUrl}${value.light}`,
+      dark: `${fullUrl}${value.dark}`
+    };
+  }
+  return value;
+};
