563eb3fe89ebe782b365c12421d551a26d5e494f
Gothsec <oscarandreshernandezpineda@gmail.com>
2024-10-25 14:18:19 -0500

    [Add] Nvidia SVG

--- src/data/svgs.ts
+++ src/data/svgs.ts
@@ -2,6 +2,12 @@ import type { iSVG } from '../types/svg';
 
 export const svgs: iSVG[] = [
   {
+    title: 'Nvidia',
+    category: 'Hardware',
+    route: '/library/nvidia.svg',
+    url: 'https://nvidia.com/'
+  },
+  {
     title: 'Google Drive',
     category: 'Google',
     route: '/library/drive.svg',
