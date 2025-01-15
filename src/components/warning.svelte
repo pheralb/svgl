3b2dc0b79dbed64cf2c49a8e6e09e548cd885b27
pheralb <pablohdez4340@gmail.com>
2025-01-15 11:40:10 +0000

    ✍️ Update warning message + add message to copy svg component

--- src/components/warning.svelte
+++ src/components/warning.svelte
@@ -5,28 +5,28 @@
   import { cn } from '@/utils/cn';
 
   let warning = false;
-  let warningName = 'svgl_warning_message';
+  let warningName = 'svgl_warn_message';
   const initialValue = browser ? window.localStorage.getItem(warningName) : true;
 </script>
 
 {#if !warning && !initialValue}
   <div
-    class="flex items-center w-full justify-between md:flex-row flex-col md:space-x-2 space-x-0 space-y-2 md:space-y-0 py-2 px-3 bg-neutral-100/60 dark:bg-neutral-800/40 text-neutral-700 dark:text-neutral-300 border-b border-neutral-200 dark:border-neutral-800"
+    class="flex w-full flex-col items-center justify-between space-x-0 space-y-2 border-b border-neutral-200 bg-neutral-100/60 px-3 py-2 text-neutral-700 dark:border-neutral-800 dark:bg-neutral-800/40 dark:text-neutral-300 md:flex-row md:space-x-2 md:space-y-0"
   >
     <div class="flex items-center space-x-2">
       <AlertTriangleIcon
         size={18}
         strokeWidth={2}
-        class="mr-1 flex-shrink-0 text-yellow-600 dark:text-yellow-500 animate-pulse"
+        class="mr-1 flex-shrink-0 animate-pulse text-yellow-600 dark:text-yellow-500"
       />
       <p>
         All SVGs include links to the respective products or companies that own them. <strong
-          >Please contact the owner directly if you intend to use their logo.</strong
+          >Please contact the owner directly if you need to use their logo.</strong
         >
-        If you are the owner of an SVG and prefer it not to be displayed here,
+        If you are the owner of an SVG and would like it removed,
         <a
           target="_blank"
-          class="underline underline-offset-4 decoration-dotted decoration-neutral-500"
+          class="underline decoration-neutral-500 decoration-dotted underline-offset-4"
           href="https://github.com/pheralb/svgl/issues/new">create an issue</a
         > on GitHub.
       </p>
