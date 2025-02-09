d5927366f885be2cc8a123cbc8e02f8bd5e549cb
Neeraj Dalal <nd941z@gmail.com>
2025-02-09 21:05:12 +0530

    fix broken links

--- src/data/svgs.ts
+++ src/data/svgs.ts
@@ -3034,7 +3034,7 @@ export const svgs: iSVG[] = [
     route: '/library/trpc.svg',
     wordmark: {
       light: '/library/trpc_wordmark_light.svg',
-      dark: 'library/trpc_wordmark_dark.svg'
+      dark: '/library/trpc_wordmark_dark.svg'
     },
     url: 'https://trpc.io/'
   },
@@ -3123,7 +3123,7 @@ export const svgs: iSVG[] = [
     },
     wordmark: {
       light: '/library/soundcloud-wordmark.svg',
-      dark: 'library/soundcloud-wordmark_dark.svg'
+      dark: '/library/soundcloud-wordmark_dark.svg'
     },
     url: 'https://soundcloud.com/'
   },
