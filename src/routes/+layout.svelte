257639c1a6a1589570ac52b849cb03f100270db7
pheralb <pablohdez4340@gmail.com>
2025-08-21 10:54:08 +0100

    🛠️ Fix import styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,8 @@
 <script lang="ts">
-  import "../app.css";
-  import favicon from "$lib/assets/favicon.svg";
+  // Styles:
+  import "@/styles/globals.css";
 
   let { children } = $props();
 </script>
-
-<svelte:head>
-  <link rel="icon" href={favicon} />
-</svelte:head>
 
 {@render children?.()}
