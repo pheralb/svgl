809d763841458d9c3e540a1a3d187954436f8d03
pheralb <pablohdez4340@gmail.com>
2025-08-21 11:58:55 +0100

    🎨 Add dialog UI component

---
+++ src/components/ui/dialog/dialog-footer.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import type { HTMLAttributes } from "svelte/elements";
+  import type { WithElementRef } from "@/types/components";
+  import { cn } from "@/utils/cn";
+
+  let {
+    ref = $bindable(null),
+    class: className,
+    children,
+    ...restProps
+  }: WithElementRef<HTMLAttributes<HTMLDivElement>> = $props();
+</script>
+
+<div
+  bind:this={ref}
+  data-slot="dialog-footer"
+  class={cn(
+    "flex flex-col-reverse gap-2 sm:flex-row sm:justify-end",
+    className,
+  )}
+  {...restProps}
+>
+  {@render children?.()}
+</div>
