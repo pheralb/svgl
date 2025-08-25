d78acff5a60985303729b5edf486afd84fa40a52
pheralb <pablohdez4340@gmail.com>
2025-08-25 19:04:40 +0100

    🎨 Add sonner UI component

---
+++ src/components/ui/sonner/sonner.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import {
+    Toaster as Sonner,
+    type ToasterProps as SonnerProps,
+  } from "svelte-sonner";
+  import { mode } from "mode-watcher";
+
+  let { ...restProps }: SonnerProps = $props();
+</script>
+
+<Sonner
+  theme={mode.current}
+  class="toaster group"
+  toastOptions={{
+    classes: {
+      toast:
+        "group toast dark:group-[.toaster]:bg-neutral-900 group-[.toaster]:font-sans",
+      description: "group-[.toast]:text-xs font-mono",
+    },
+  }}
+  {...restProps}
+/>
