cf3918376fc0779a8524183ee9f3361228cc4788
pheralb <pablohdez4340@gmail.com>
2025-08-25 19:07:50 +0100

    🛠️ Create custom globals config file with app data

---
+++ src/globals.ts
@@ -0,0 +1,7 @@
+export const globals = {
+  githubUrl: "https://github.com/pheralb/svgl",
+  twitterUrl: "https://x.com/pheralb_",
+  submitUrl:
+    "https://github.com/pheralb/svgl?tab=readme-ov-file#-getting-started",
+  currentVersion: "beta",
+};
