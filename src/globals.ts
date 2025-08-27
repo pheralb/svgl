d7500fe56e70cdc71a19bac3f6711b53d1f0bed3
pheralb <pablohdez4340@gmail.com>
2025-08-27 15:33:01 +0100

    ⚙️ Add ``apiGithubUrl`` property

--- src/globals.ts
+++ src/globals.ts
@@ -1,5 +1,6 @@
 export const globals = {
   githubUrl: "https://github.com/pheralb/svgl",
+  apiGithubUrl: "https://api.github.com/repos/pheralb/svgl",
   twitterUrl: "https://x.com/pheralb_",
   submitUrl:
     "https://github.com/pheralb/svgl?tab=readme-ov-file#-getting-started",
