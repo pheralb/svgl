e2c2ca9c3079a6d28c13723ba18e99f3ba894299
pheralb <pablohdez4340@gmail.com>
2025-08-29 14:05:05 +0100

    📦 Add @sveltejs/adapter-node dependency + config

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
-import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+
+// Adapter:
+import adapter from "@sveltejs/adapter-node";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
