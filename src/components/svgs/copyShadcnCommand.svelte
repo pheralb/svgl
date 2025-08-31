73bd5a4f782d0fd32b99db90e85f8344fdbfe5aa
pheralb <pablohdez4340@gmail.com>
2025-08-31 13:43:17 +0100

    🛠️ Create codeBlock UI component + setup shadcn/ui registry

--- src/components/svgs/copyShadcnCommand.svelte
+++ src/components/svgs/copyShadcnCommand.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-  import { clipboard } from "@/utils/clipboard";
-
-  import CopyIcon from "@lucide/svelte/icons/copy";
   import { Button } from "@/components/ui/button";
+  import Shadcn from "@/components/logos/shadcn.svelte";
   import SelectPkgManager from "@/components/selectPkgManager.svelte";
+
   import { pkgManager, type PackageManager } from "@/stores/pkgManager.store";
-  import Shadcn from "../logos/shadcn.svelte";
+  import CodeBlock from "@/components/codeBlock.svelte";
+  import SetupShadcnRegistry from "@/components/svgs/setupShadcnRegistry.svelte";
 
   interface Props {
     svgTitle: string;
@@ -26,24 +26,14 @@
     .toLowerCase()
     .replace(/\s+/g, "-")
     .replace(/[^a-z0-9-]/g, "");
-
-  const handleCopy = () => {
-    clipboard(`${shadcnCommand} @svgl/${svgFormatTitle}`);
-  };
 </script>
 
 <div class="flex items-center justify-between space-x-2">
+  <SetupShadcnRegistry>
+    <Button variant="outline" size="sm">
+      <span>Setup Registry</span>
+    </Button>
+  </SetupShadcnRegistry>
   <SelectPkgManager />
-  <Button variant="outline" onclick={handleCopy} size="sm">
-    <CopyIcon size={14} />
-    <span>Copy</span>
-  </Button>
 </div>
-<div
-  class="flex items-center space-x-2 rounded-md border border-neutral-200 p-2.5 dark:border-neutral-800"
->
-  <Shadcn size={14} />
-  <code class="font-mono text-sm select-all">
-    {shadcnCommand} @svgl/{svgFormatTitle}
-  </code>
-</div>
+<CodeBlock code={`${shadcnCommand} @svgl/${svgFormatTitle}`} Icon={Shadcn} />
