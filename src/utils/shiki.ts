ea4b598f2f3b3a68cbb1a9bf8337867a7f3bfc66
pheralb <pablohdez4340@gmail.com>
2025-09-05 11:27:00 +0100

    ✨ Update Shiki configuration to include JSON and HTML languages

--- src/utils/shiki.ts
+++ src/utils/shiki.ts
@@ -11,8 +11,10 @@ import githubLight from "@shikijs/themes/github-light";
 import githubDark from "@shikijs/themes/github-dark";
 
 // Languages:
+import html from "@shikijs/langs/html";
 import ts from "@shikijs/langs/ts";
 import bash from "@shikijs/langs/bash";
+import json from "@shikijs/langs/json";
 
 let jsEngine: RegexEngine | null = null;
 let highlighter: Promise<HighlighterCore> | null = null;
@@ -29,13 +31,13 @@ const rehypeShikiOptions: RehypeShikiOptions = {
     light: "github-light",
     dark: "github-dark",
   },
-  langs: [bash, ts],
+  langs: [bash, ts, json, html],
 };
 
 const shikiHighlighter = async (): Promise<HighlighterCore> => {
   highlighter ??= createHighlighterCore({
     themes: [githubLight, githubDark],
-    langs: [bash, ts],
+    langs: [bash, ts, json, html],
     engine: getShikiEngine(),
   });
   return highlighter;
