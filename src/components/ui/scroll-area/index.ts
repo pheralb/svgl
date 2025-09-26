e8d42a7055caf3eddadbde9c92099e55ee4f3778
SameerJS6 <singh6.sameer@gmail.com>
2025-09-26 08:59:35 +0530

    ✨ Introduce ScrollArea components for structured scroll functionality

---
+++ src/components/ui/scroll-area/index.ts
@@ -0,0 +1,13 @@
+import Scrollbar from "./scroll-area-scrollbar.svelte";
+import Root from "./scroll-area.svelte";
+import Mask from "./scroll-area-mask.svelte";
+
+export {
+	Root,
+	Scrollbar,
+	Mask,
+  // 
+	Root as ScrollArea,
+	Scrollbar as ScrollAreaScrollbar,
+	Mask as ScrollAreaMask,
+};
