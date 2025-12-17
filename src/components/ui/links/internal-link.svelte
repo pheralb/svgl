aa6c2345e4b7b5ca384048b4716a7b63cebec1b8
pheralb <pablohdez4340@gmail.com>
2025-12-17 14:00:11 +0000

    fix: create custom <InternalLink /> & <ExternalLink /> components

---
+++ src/components/ui/links/internal-link.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import type { LinkProps } from "@/components/ui/links/links.types";
+  import { cn } from "@/utils/cn";
+
+  interface InternalLinkProps extends LinkProps {
+    preloadData?: boolean;
+  }
+
+  const {
+    href,
+    label,
+    title,
+    className,
+    children,
+    preloadData,
+  }: InternalLinkProps = $props();
+</script>
+
+<a
+  {href}
+  {title}
+  aria-label={label}
+  class={cn(className)}
+  data-sveltekit-preload-data={preloadData}
+>
+  {#if children}
+    {@render children()}
+  {/if}
+</a>
