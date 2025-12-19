e50a5846a2e081acded6dc8b6190beea54a20e5a
pheralb <pablohdez4340@gmail.com>
2025-12-19 09:46:42 +0000

    chore: add vite-plugin-devtools-json + config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import { defineConfig } from "vite";
 // Plugins:
 import tailwindcss from "@tailwindcss/vite";
 import { sveltekit } from "@sveltejs/kit/vite";
+import devtoolsJson from "vite-plugin-devtools-json";
 import contentCollections from "@content-collections/vite";
 
 export default defineConfig({
-  plugins: [tailwindcss(), sveltekit(), contentCollections()],
+  plugins: [devtoolsJson(), tailwindcss(), sveltekit(), contentCollections()],
 });
