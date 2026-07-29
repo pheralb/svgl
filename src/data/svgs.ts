3e9282cb87da01bfc857b9099b3901421c77c5d2
sharadpatel <patel.k.sharad@gmail.com>
2026-07-29 15:35:16 +0800

    feat(hubspot): add hubspot logo;

--- src/data/svgs.ts
+++ src/data/svgs.ts
@@ -5130,4 +5130,10 @@ export const svgs: iSVG[] = [
     url: "https://grida.co",
     brandUrl: "https://grida.co/brand",
   },
+  {
+    title: "HubSpot",
+    category: ["Software"],
+    route: "/library/hubspot.svg",
+    url: "https://www.hubspot.com/",
+  },
 ];
