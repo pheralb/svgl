e58224720d7b6111d5572a5e8883e2cfc972ab0e
Jiri Spac <capajj@gmail.com>
2026-09-01 03:19:37 +0200

    feat: add Authier logo

--- src/data/svgs.ts
+++ src/data/svgs.ts
@@ -2,6 +2,12 @@ import type { iSVG } from "@/types/svg";
 
 export const svgs: iSVG[] = [
   {
+    title: "Authier",
+    category: ["Software", "Authentication"],
+    route: "/library/authier.svg",
+    url: "https://www.authier.pm/",
+  },
+  {
     title: 'Ossium',
     category: 'Software',
     route: '/library/ossium_logo.svg',
